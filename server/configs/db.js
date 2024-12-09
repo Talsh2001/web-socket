afb11c67be1f922053d64afe17d61268660b3add
Tal Shalev <ykaku1234@gmail.com>
2024-12-09 18:43:07 +0200

    sg

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = () => {
   mongoose
     .connect(
-      "mongodb+srv://tomq5555:OZGQhWZcZmqmsOfh@cluster0.c3lnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/socketDB"
+      "mongodb+srv://tomq5555:Talsh7410@cluster0.c3lnb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => {
       console.log("Connected to DB");
