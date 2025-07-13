1c11d51221fae325d99538c693d09f11f334dd41
Tal Shalev <ykaku1234@gmail.com>
2025-07-13 20:03:29 +0300

    Initial commit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 4000;
 
 connectDB();
 app.use(cors());
